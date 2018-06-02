50754c863be26521ec064f459123e1da3de714ac
deepanprabu <deepanprabhu1986@gmail.com>
2018-06-02 21:38:43 +0530

    DoWhileProgram

---
+++ src/com/basic/DoWhileProgram.java
@@ -0,0 +1,12 @@
+package com.basic;
+
+public class DoWhileProgram {
+	public static void main(String args[]) {
+		int b=10;
+		do {
+			System.out.println("the value is "+b);
+			b++;
+		}while(b<21);
+	}
+
+}
