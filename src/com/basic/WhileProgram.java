d99d84a3329463444d88cb87cae215a3e668270b
deepanprabu <deepanprabhu1986@gmail.com>
2018-06-02 21:38:23 +0530

    WhileProgram

---
+++ src/com/basic/WhileProgram.java
@@ -0,0 +1,12 @@
+package com.basic;
+
+public class WhileProgram {
+	public static void main(String args[]) {
+		int i=5;
+		while(i<11) {
+			System.out.println("The value is"+i);
+			i++;
+		}
+	}
+
+}
