bf05e1e2beaf828252b3f693332909e0c50bd606
deepanprabu <deepanprabhu1986@gmail.com>
2018-06-02 22:44:01 +0530

    Invoice class program

---
+++ src/com/basic/Invoice.java
@@ -0,0 +1,57 @@
+package com.basic;
+
+public class Invoice {
+	String partNumber;
+	String partDescripton;
+	Double priceperItem;
+	int itemQuantity;
+	public String getPartNumber() {
+		return partNumber;
+	}
+	public void setPartNumber(String partNumber) {
+		this.partNumber = partNumber;
+	}
+	public String getPartDescripton() {
+		return partDescripton;
+	}
+	public void setPartDescripton(String partDescripton) {
+		this.partDescripton = partDescripton;
+	}
+	public Double getPriceperItem() {
+		return priceperItem;
+	}
+	public void setPriceperItem(Double priceperItem) {
+		this.priceperItem = priceperItem;
+		if(this.priceperItem<0) {
+			this.priceperItem=0.0;
+		}
+		else
+		{
+		this.priceperItem = priceperItem;
+	}
+	}
+	public int getItemQuantity() {
+		return itemQuantity;
+	}
+	public void setItemQuantity(int itemQuantity) {
+		this.itemQuantity = itemQuantity;
+		if(this.itemQuantity<0) {
+			this.itemQuantity=0;
+		}else {
+			
+		
+		this.itemQuantity = itemQuantity;
+	}
+	}
+	public double getInvoiceAmount() {
+		return this.itemQuantity*this.priceperItem;
+	}
+public static void main (String args[]) {
+	Invoice i1=new Invoice();
+	i1.setItemQuantity(12);
+	i1.setPriceperItem(10.0);
+	System.out.println("invoice amount is"+i1.getInvoiceAmount());
+	
+	
+}
+}
