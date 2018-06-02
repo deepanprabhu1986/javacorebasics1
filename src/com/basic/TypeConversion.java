bb49b99e9b3d44f85f0ed41408cc77a18d8f50a7
deepanprabu <deepanprabhu1986@gmail.com>
2018-06-02 20:33:57 +0530

    TypeConversion

---
+++ src/com/basic/TypeConversion.java
@@ -0,0 +1,13 @@
+package com.basic;
+
+public class TypeConversion {
+	public static void main (String[] args) {
+	byte a=10;
+	byte b=20;
+	int c=a+b;
+System.out.println("sum is "+c);
+
+
+	}
+
+}
