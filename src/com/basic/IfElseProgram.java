e93a66c851c5811c89bf7ce01777117c6d5b5810
deepanprabu <deepanprabhu1986@gmail.com>
2018-06-02 21:37:25 +0530

    IfElseProgram

---
+++ src/com/basic/IfElseProgram.java
@@ -0,0 +1,17 @@
+package com.basic;
+
+public class IfElseProgram {
+	public static void main(String args[]) {
+		int a=10;
+		int b=20;
+		if(a>b)
+		{
+			System.out.println("ä is bigger value");
+		}
+		else
+		{
+			System.out.println("b is bigger");
+		}
+	}
+
+}
